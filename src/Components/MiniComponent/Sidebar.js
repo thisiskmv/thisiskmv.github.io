6de5f2b03f5eb891724b2f501dbc7e6207d50d64
kamal verma <122443494+thisiskmv@users.noreply.github.com>
2024-09-12 18:16:48 +0530

    Update Sidebar.js

--- src/Components/MiniComponent/Sidebar.js
+++ src/Components/MiniComponent/Sidebar.js
@@ -22,7 +22,6 @@ const Sidebar = () => {
 
         <Box sx={{position:"fixed",bottom:"5%",left:"4%",display:"grid",justifyContent:"center",alignContent:"center",gap:"15px"}}>
             <a id="contact-github" className='socialLogo' target={"_blank"} href={details.githubLink} style={{textDecoration:"none",color:"white",fontSize:"1.7rem"}}><i className="fa-brands fa-github"></i></a>
-            <a  id="contact-linkedin" className='socialLogo' target={"_blank"} href={details.linkedInLink} style={{textDecoration:"none",color:"white",fontSize:"1.7rem"}}><i className="fa-brands fa-linkedin"></i></a>
             <a id="contact-phone" className='socialLogo' target={"_blank"} href={details.instagramLink} style={{textDecoration:"none",color:"white",fontSize:"1.7rem"}}><i className="fa-solid fa-phone"></i></a>
             <a id="contact-email" className='socialLogo' target={"_blank"} href={details.mailLink} style={{textDecoration:"none",color:"white",fontSize:"1.5rem"}}><i className="fa-regular fa-envelope"></i></a>
             
